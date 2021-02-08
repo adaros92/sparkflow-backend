30d5730363e36fc10810a2c6ce8217759ffa4619
adamsrosales <adams.rosales.92@gmail.com>
2021-02-07 19:31:21 -0800

    Add index.html and bootstrap styling

--- sparkflowserver/views.py
+++ sparkflowserver/views.py
@@ -1,6 +1,9 @@
+from flask import url_for, render_template
+
 from sparkflowserver import app
 
 
 @app.route('/')
+@app.route('/index.html')
 def index():
-    return 'Hello World!'
+    return render_template('index.html')
